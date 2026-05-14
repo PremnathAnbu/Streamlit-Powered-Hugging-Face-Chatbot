20ec4af7f3267bb503b4fcf9e16f6fd1a215f6a4
unknown love <premnath97100@gmail.com>
2026-05-15 00:19:01 +0530

    changes done in yaml file

--- src/langgraphagenticai/ui/uiconfigfile.py
+++ src/langgraphagenticai/ui/uiconfigfile.py
@@ -1,18 +1,46 @@
 from configparser import ConfigParser
+from pathlib import Path
+
 
 class Config:
-    def __init__(self,config_file="C:/agentic ai/huggingface project/langchainproject/src/langgraphagenticai/ui/uiconfigfile.ini"):
-        self.config=ConfigParser()
+
+    def __init__(self):
+
+        config_file = Path(__file__).parent / "uiconfigfile.ini"
+
+        self.config = ConfigParser()
         self.config.read(config_file)
-        
+
     def get_llm_options(self):
-        return self.config["DEFAULT"].get("LLM_OPTIONS").split(", ")
-    
+
+        options = self.config["DEFAULT"].get(
+            "LLM_OPTIONS",
+            "GROQ"
+        )
+
+        return [option.strip() for option in options.split(",")]
+
     def get_usecase_options(self):
-        return self.config["DEFAULT"].get("USECASE_OPTIONS").split(", ")
-    
+
+        options = self.config["DEFAULT"].get(
+            "USECASE_OPTIONS",
+            "Chatbot"
+        )
+
+        return [option.strip() for option in options.split(",")]
+
     def get_groq_model_options(self):
-        return self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS").split(", ")
-    
+
+        options = self.config["DEFAULT"].get(
+            "GROQ_MODEL_OPTIONS",
+            "llama3-8b-8192"
+        )
+
+        return [option.strip() for option in options.split(",")]
+
     def get_page_title(self):
-        return self.config["DEFAULT"].get("PAGE_TITLE")
+
+        return self.config["DEFAULT"].get(
+            "PAGE_TITLE",
+            "LangGraph Agentic AI"
+        )
