b599371cf2db9fda8470c0fde8a3fa5ed092eb32
unknown love <premnath97100@gmail.com>
2026-05-15 01:39:28 +0530

    tavily usecase added

--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -48,8 +48,8 @@ class GraphBuilder:
         
         self.graph_builder.add_edge(START,"chatbot")
         self.graph_builder.add_conditional_edges("chatbot",tools_condition)
-        self.graph_builder.add_edge("tools","chatbot")
-        self.graph_builder.add_edge("chatbot",END)
+        self.graph_builder.add_edge("tools",END)
+        
         
         
         
